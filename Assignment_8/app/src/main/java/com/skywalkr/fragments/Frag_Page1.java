e7d2c761a1d38e55d0842aa40b48db549240e7e4
Skywalkr08 <prasannkavlekar9@gmail.com>
2020-10-16 13:03:18 +0530

    Added files

---
+++ Assignment_8/app/src/main/java/com/skywalkr/fragments/Frag_Page1.java
@@ -0,0 +1,41 @@
+package com.skywalkr.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Frag_Page1 extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+    private String mParam1;
+    private String mParam2;
+
+    public Frag_Page1() {
+    }
+
+    public static Frag_Page1 newInstance() {
+        Frag_Page1 fragment = new Frag_Page1();
+        Bundle args = new Bundle();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fpage1, container, false);
+    }
+}
---
+++ Assignment_8/app/src/main/java/com/skywalkr/fragments/User.java
@@ -0,0 +1,5 @@
+package com.skywalkr.fragments;
+
+public class User {
+    String name, phone, email, pass, confpass,rbtn, sval, dval, bval;
+}
